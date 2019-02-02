fc0674879bb781c5218336ed22dcc3973d79b0b9
Ellie Peeva <elisaveta.peeva@gmail.com>
2019-02-02 16:12:47 +0200

    Ellie's class

---
+++ src/ImageEncrypter.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class ImageEncrypter {
+	
+	public void readImage() throws IOException {
+		
+		// width and height of the image
+		int width = 600;
+		int height = 447;
+		
+		// For storing image in RAM 
+        BufferedImage image = null; 
+        
+        try {
+        	
+        }
+        catch
+	}
+	
+}
+
