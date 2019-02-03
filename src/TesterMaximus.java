349cdf444f930f91903586ad346d4a1c114361b2
maxwillkelly <maxwill.kelly@gmail.com>
2019-02-03 08:13:45 +0000

    Changes Test file

--- src/TesterMaximus.java
+++ src/TesterMaximus.java
@@ -1,12 +1,12 @@
 
 public class TesterMaximus {
 //
-//	public static void main(String[] args) {
-//		Tester testObj = new Tester();
-//		testObj.maxTests();
-//	}
+	public static void main(String[] args) {
+		TesterMaximus testObj = new TesterMaximus();
+		testObj.maxTests();
+	}
 //	
-//	public void maxTests() {
+	public void maxTests() {
 //		String[] encryptionArray = {" ","-35,46,21,74,-32,53,-2"};
 //		TextToRGBA textToRGBAObj = new TextToRGBA();
 //		int[] keyRGBArray = textToRGBAObj.getTextRGB(encryptionArray);
@@ -14,11 +14,25 @@ public class TesterMaximus {
 //		for (int i=0;i<keyRGBArray.length;i++) {
 //			System.out.printf(keyRGBArray[i] + ",");
 //		}
-//		RGBAToText rgbToTextObj = new RGBAToText();
-//		String text = rgbToTextObj.getText();
-//		String key = rgbToTextObj.getKey();
+		RGBAToText rgbToTextObj = new RGBAToText();
+		int[] textArray = {};
+		int[][] keyArray = {
+			
+		};
+		
+		String[] text = rgbToTextObj.getText(textArray);
+		String[] key = rgbToTextObj.getKey(keyArray);
+		
+		for (int i=0;i<key.length;i++) {
+			System.out.printf(key[i] + ",");
+		}
+		
+		System.out.println("");
+		for (int i=0;i<text.length;i++) {
+			System.out.printf(text[i] + ",");
+		}
 //		System.out.println(text);
 //		System.out.println(key);
-//	}
+	}
 
 }
